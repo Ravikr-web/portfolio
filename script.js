53f1a20da465e0bbbdefbadc1403ad8468cda9da
Ravikr-web <bepositive2822@gmail.com>
2025-03-18 23:44:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll(".fade-in");
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.5 });
+    elements.forEach(el => observer.observe(el));
+});
